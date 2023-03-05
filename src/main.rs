aa4124dceb6b268557127557d25c919252091f6b
Mary Strodl <ipadlover8322@gmail.com>
2023-03-04 21:06:58 -0500

    add env logger

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use crate::protobuf_route::protobuf_route;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+  env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
   let mut app = tide::new();
+  app.with(tide::log::LogMiddleware::new());
   app.at("/rt/:agency_id/:agency_code").get(protobuf_route);
   let addr = "0.0.0.0:6969";
   println!("Ready to go at: http://{}", addr);
